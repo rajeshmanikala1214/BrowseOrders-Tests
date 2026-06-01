0248188dfe7f4d364e7eb1247518c17628e68151
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-06-01 15:37:17 +0530

    Added karma.conf.js and AllTests and AllJourney files

---
+++ webapp/test/integration/AllJourneys.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/demo/orderbrowser/test/integration/MasterJourney",
+    "sap/ui/demo/orderbrowser/test/integration/NavigationJourney",
+    "sap/ui/demo/orderbrowser/test/integration/NavigationJourneyPhone",
+    "sap/ui/demo/orderbrowser/test/integration/NotFoundJourney",
+    "sap/ui/demo/orderbrowser/test/integration/NotFoundJourneyPhone"
+], function() {
+    "use strict";
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/demo/orderbrowser/test/unit/controller/Detail.controller",
+    "sap/ui/demo/orderbrowser/test/unit/controller/ListSelector",
+    "sap/ui/demo/orderbrowser/test/unit/helper/FakeI18nModel",
+    "sap/ui/demo/orderbrowser/test/unit/model/formatter",
+    "sap/ui/demo/orderbrowser/test/unit/model/models"
+], function() {
+    "use strict";
+});
