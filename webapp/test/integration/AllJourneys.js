75b122a916ccad750d1a2a4b36974ce01a2a3c82
rajeshmanikala1214 <rajesh.manikala812@gmail.com>
2026-07-02 13:40:40 +0530

    Cladue suggested some change introduced them

--- webapp/test/integration/AllJourneys.js
+++ webapp/test/integration/AllJourneys.js
@@ -1,9 +1,16 @@
 sap.ui.define([
+    "sap/ui/test/Opa5",                                                     // add
+    "sap/ui/demo/orderbrowser/test/integration/arrangements/Startup",       // add
     "sap/ui/demo/orderbrowser/test/integration/MasterJourney",
     "sap/ui/demo/orderbrowser/test/integration/NavigationJourney",
     "sap/ui/demo/orderbrowser/test/integration/NavigationJourneyPhone",
     "sap/ui/demo/orderbrowser/test/integration/NotFoundJourney",
     "sap/ui/demo/orderbrowser/test/integration/NotFoundJourneyPhone"
-], function() {
+], function(Opa5, Startup) {                                                // add params
     "use strict";
+    Opa5.extendConfig({                                                     // add block
+        arrangements: new Startup(),
+        viewNamespace: "sap.ui.demo.orderbrowser.view.",
+        autoWait: true
+    });
 });
